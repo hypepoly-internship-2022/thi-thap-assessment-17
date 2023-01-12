67293d1bd85cb2d33ef9d9242a6553f98a7b276c
Thap <dangthithap9@gmail.com>
2023-01-12 11:24:50 +0700

    init

--- Assets/Scenes/Script/Rotate.cs
+++ Assets/Scenes/Script/Rotate.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
        transform.Rotate(new Vector3(0, 10, 0) * Time.deltaTime); 
